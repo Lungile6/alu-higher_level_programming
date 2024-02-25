a933fe5711b5dfa8a66d706ebc988893edd40d03
Lungile <l.mabelebel@alustudent.com>
2024-02-25 08:14:22 -0800

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     if not sentence:
         sentence = None
     if sentence:
-        sen_len = 0
+        sen_len = len(sentence)
     else:
         sen_len = 0
     return (sen_len, sentence if not sentence else sentence[:1])
