522a0d29352bd80110a53ffbb12825a75063902c
Lungile <l.mabelebel@alustudent.com>
2024-02-25 07:24:38 -0800

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,9 +4,9 @@ def print_matrix_integer(matrix=[[]]):
         print()
     else:
         for row in range(len(matrix)):
-            for item in range(len(matrix)):
+            for item in range(len(matrix[row])):
                 if item != len(matrix[row]) - 1:
-                    endspace = ''
+                    endspace = ' '
                 else:
                     endspace = ''
                 print("{:d}".format(matrix[row][item]), end=endspace)
