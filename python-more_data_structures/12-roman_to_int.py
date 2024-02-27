29f91f65cecf6c467942c7450d196701fa54b22a
Lungile <l.mabelebel@alustudent.com>
2024-02-27 10:13:51 -0800

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if roman_string is None or roman_string is str():
+def roman_to_int(roman_string: str):
+    if roman_string is None or not isinstance(roman_string, str)
+    or not roman_string:
         return 0
-    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 50, 'M': 1000}
-    numbers = [data[x] for x in roman_string] + [0]
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     rep = 0
-    for i in range(len(numbers) - 1):
-        if numbers[i] >= numbers[i+1]:
-            rep += numbers[i]
+    for i in range(len(roman_string) - 1):
+        if roman_string[i] not in data or roman_string[i+1] not in data:
+            return 0  # Invalid Roman numeral characters
+        if data[roman_string[i]] >= data[roman_string[i+1]]:
+            rep += data[roman_string[i]]
         else:
-            rep -= numbers[i]
+            rep -= data[roman_string[i]]
+    # Add the value of the last Roman numeral character
+    rep += data[roman_string[-1]]
     return rep
