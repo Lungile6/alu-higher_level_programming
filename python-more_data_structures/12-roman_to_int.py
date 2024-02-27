4217a487892b970fd71decafe52dc80706c765c9
Lungile <l.mabelebel@alustudent.com>
2024-02-27 10:16:54 -0800

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string: str):
-    if roman_string is None or not isinstance(roman_string, str)
-    or not roman_string:
+    if roman_string is None or roman_string is str() or not roman_string:
         return 0
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     rep = 0
