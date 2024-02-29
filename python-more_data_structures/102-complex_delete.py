473a91cc79ccbe8d2b3ce0841b6eea254f77de68
Lungile <l.mabelebel@alustudent.com>
2024-02-28 23:22:05 -0800

    102-complex_delete.py

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     tmp = a_dictionary.copy()
-    for k, v in tmp.items():
-        if value == v:
-            a_dictionary.pop(k)
+    for keys, v in tmp.items():
+        if v == value:
+            a_dictionary.pop(keys)
         return a_dictionary
