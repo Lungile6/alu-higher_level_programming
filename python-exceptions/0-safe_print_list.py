4536199d3f87ae006dbda01a9daab120b2f926a2
Lungile <l.mabelebel@alustudent.com>
2024-02-29 13:56:50 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     num = 0
-    for item in range(x):
-        try:
-            print("{}".format(my_list[item]), end"")
+    try:
+        for item in range(x):
+            print("{}".format(my_list[item]), end='')
             num += 1
-        except IndexError:
-            pass
-        print("")
-        return (num)
+    except IndexError:
+        pass
+    print()
+    return num
