dff6d382487338fbdaa9a36615313b1225aebe61
Lungile <l.mabelebel@alustudent.com>
2024-02-29 15:07:33 -0800

    4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for item in range(list_lenght):
+    for item in range(list_length):
         try:
             div = my_list_1[item] / my_list_2[item]
         except TypeError:
