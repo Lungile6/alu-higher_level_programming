caca66900dc5f8eec2fdd95c34fa2815e3cee343
Lungile <l.mabelebel@alustudent.com>
2024-03-06 07:50:03 -0800

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a  Square"""
+
+
+class Square:
+    """ Represents a Square"""
+    def __init__(self, int):
+        """Initialize a new square.
+        Args:
+          size(int): The size of new square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must ba an integer")
+        elif size < 0:
+            raise ValueError("size must ba >= 0")
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
