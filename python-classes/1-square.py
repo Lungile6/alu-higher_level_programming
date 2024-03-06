8ab14c5250547d17cf6d14793ee459503369d7bb
Lungile <l.mabelebel@alustudent.com>
2024-03-06 06:58:44 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,12 +4,9 @@
 
 class Square:
     """Defines a square."""
-    
-
     def __init__(self, size):
         """Constructor.
         Args:
           size: Lenght of side of the square.
         """
-
         self.__size = size
