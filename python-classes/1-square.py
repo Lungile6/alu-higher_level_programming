1560fc5c0d069d61fa25fce1574104a330dbcdf0
Lungile <l.mabelebel@alustudent.com>
2024-03-06 06:53:02 -0800

    Documenting README.md file

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a square."""
+    
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+          size: Lenght of side of the square.
+        """
+
+        self.__size = size
