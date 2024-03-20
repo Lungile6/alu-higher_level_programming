aa3871384f6c581b4b05c046bbb63d589ca76651
Lungile <l.mabelebel@alustudent.com>
2024-03-20 10:51:24 -0700

    Done

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- Creates the table force_name on my MySQL server
 -- Creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name ('id' INT, 'name' VAARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
--- Creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on my MySQL server.
--- Creates a database
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Use a database
+-- use a database
 USE hbtn_0d_usa;
--- Creates a table
+-- creates a table
 CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
-       	NAME VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES states(id));
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
