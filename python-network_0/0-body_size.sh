64b28621637edc4c3166e1d849de8e3bbc828564
Lungile <l.mabelebel@alustudent.com>
2024-03-22 07:45:48 -0700

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Check if the user provided a URL as an argument.
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
 # Send a request to the specified URL and display the size of the response body in bytes
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
