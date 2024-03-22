4efc6ba1959c76fadf0be8347d12ac85b1cc5896
Lungile <l.mabelebel@alustudent.com>
2024-03-22 10:26:36 -0700

    1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the specified URL and display the body of the response.
-curl -s "$1" -o response_body -w '%{http_code}' | grep -q '^200$' && cat response_body; rm -f response_body
+status=$(curl -s -o response_body -w "%{http_code}" "$1") [ "$status" -eq 200 ] && cat response_body; rm -f response_body
