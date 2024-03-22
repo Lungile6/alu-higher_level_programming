90c5dad52d49a7612a7800197b513c4b1ff9249d
Lungile <l.mabelebel@alustudent.com>
2024-03-22 08:05:33 -0700

    1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a Get to the spesified URL and display the body of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && -s "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
