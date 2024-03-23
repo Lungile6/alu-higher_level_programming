bbfd3fafed28de57b90bb806bf2f34b14e7c1603
Lungile <l.mabelebel@alustudent.com>
2024-03-23 06:46:15 -0700

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the passed URL, and displays the body of the response.
-curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" \
--d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" \ -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
