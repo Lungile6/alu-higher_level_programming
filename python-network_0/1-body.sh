761ea534472504a15dc06d7dae068bc0f9db85c7
Lungile <l.mabelebel@alustudent.com>
2024-03-23 16:47:30 -0700

    I hope all are fixed!

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the specified URL and display the body of the response.
-status=$(curl -s -o response_body -w "%{http_code}" "$1") [ "$status" -eq 200 ] && cat response_body; rm -f response_body
+curl -sLfG "$1"
--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the specified URL with a custom header and display the body of the reponse.
-curl -s -L -w "%{http_code}\n" -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sG "$1" -H "X-HolbertonSchool-User-Id: 98"
