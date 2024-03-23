66c3daa8759fbc2cab1b27984d51f2ba783815bf
Lungile <l.mabelebel@alustudent.com>
2024-03-23 13:05:28 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,12 +2,18 @@
 import urllib.request
 
 
+# Define the target URL
 url = 'https://alu-intranet.hbtn.io/status'
 
+# Open the URL and retrieve the response
 with urllib.request.urlopen(url) as response:
+    # Read the content of the response (in bytes)
     content = response.read()
+
+    # Decode the content to UTF-8 to get a human-readable string
     utf8_content = content.decode('utf-8')
 
+# Display the response details
 print("Body response:")
 print(f"    - type: {type(content)}")
 print(f"    - content: {content}")
