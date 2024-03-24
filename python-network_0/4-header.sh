7fb48d7772cb3e47939cb744fd27aed80295f4f2
Lungile <l.mabelebel@alustudent.com>
2024-03-24 12:15:56 -0700

    Debug

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the specified URL with a custom header and display the body of the reponse.
-curl -sG -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s -H "X-School-User-Id":98 "$1"
