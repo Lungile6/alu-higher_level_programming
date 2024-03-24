6aa291890a1df48d73ae44e9e825d7a8acffcc63
Lungile <l.mabelebel@alustudent.com>
2024-03-24 13:01:41 -0700

    100-github_commits.py

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -3,20 +3,18 @@
 
 Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-import sys
+
 import requests
+import sys
 
+if __name__ == "__main":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    response = requests.get(url)
+    commits = response.json()
 
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    for commit in commits[:10]:
+        sha = commit.get("sha")
+        author_name = commit.get("commit").get("author").get("name")
+        print(f"{sha}: {author_name}")
