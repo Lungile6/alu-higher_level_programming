077cd52a4c96058cec1a8fbd33840174bd8243d4
Lungile <l.mabelebel@alustudent.com>
2024-03-24 02:53:51 -0700

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the specified URL with a custom header and display the body of the reponse.
-curl -sGH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sG "$1" -H "X-HolbertonSchool-User-Id: 98" || echo "Error: cURL request failed"
