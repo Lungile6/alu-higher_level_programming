5067c6a574d040f817e1b8679bfd529fda2304d2
Lungile6 <l.mabelebel@alustudent.com>
2024-05-28 15:22:28 -0700

    Done

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const arg1 = process.argv[2] || 'undefined';
+const arg2 = process.argv[3] || 'undefined';
 
-if (arg1 !== undefined && arg2 !== undefined) {
-  console.log(`${arg1} is ${arg2}`);
-} else {
-  console.log('undefined is undefined');
-}
+console.log(`${arg1} is ${arg2}`);
