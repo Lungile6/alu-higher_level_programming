b42bf3dd3eedb9bff4a0cb3d7d1944cad80444cf
Lungile6 <l.mabelebel@alustudent.com>
2024-06-02 04:17:08 -0700

    Done D

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      // Initialize instance attributes if w and h are positive integers
-      this.width = w;
-      this.height = h;
-    } else {
-      // Throw an error if w or h is not a positive integer
-      throw new Error('Width and height must be positive integers');
-    }
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 }
-
-module.exports = Rectangle;
+module.exports = Square;
