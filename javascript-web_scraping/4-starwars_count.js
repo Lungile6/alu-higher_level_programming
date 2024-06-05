43295b08fed8bc22b5234f50fc072d3d150c8fd7
Lungile6 <l.mabelebel@alustudent.com>
2024-06-05 16:04:37 -0700

    Debugging

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = process.argv[2]; // Get the API URL from command line arguments
-const characterId = 18; // Character ID for Wedge Antilles
+let nFilms = 0;
 
-// Make a GET request to the Star Wars API
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error); // Print the error object if the request fails
-  } else {
-    const filmsData = JSON.parse(body);
-    const filmsWithWedge = filmsData.results.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(filmsWithWedge.length); // Print the number of movies
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          nFilms++;
+        }
+      }
+    }
   }
+  console.log(nFilms);
 });
--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
-const url = process.argv[2]; // Get the URL from command line arguments
-const filePath = process.argv[3]; // Get the file path to store
-
-// Make a GET request to the specified URL
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error); // Print the error object if the request fails
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err); // Print the error object if writing fails
-      } else {
-        console.log(`Content written to ${filePath}`);
-      }
-    });
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
   }
 });
