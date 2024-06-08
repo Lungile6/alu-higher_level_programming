8422a5489f6fa2fc5cd00266f3169b9187b34478
Lungile6 <l.mabelebel@alustudent.com>
2024-06-08 14:07:13 -0700

    Done

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,63 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      // Initialize instance attributes if w and h are positive integers
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
-    } else {
-      // Handle the case when w or h is not a positive integer
-      throw new Error('Width and height must be positive integers');
     }
   }
 
   print () {
-    // Print the rectangle using the character 'X'
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
 
   rotate () {
-    // Exchange width and height
-    [this.width, this.height] = [this.height, this.width];
+    const exchange = this.width;
+    this.width = this.height;
+    this.height = exchange;
   }
 
   double () {
-    // Multiply width and height by 2
     this.width *= 2;
     this.height *= 2;
   }
 }
 
-module.exports = Rectangle;
+module.exports = Rectangle;#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const exchange = this.width;
+    this.width = this.height;
+    this.height = exchange;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
